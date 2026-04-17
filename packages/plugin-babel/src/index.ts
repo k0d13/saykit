9a7c99f0607c1d2d406dfa3fd8489685c4170d98
k0d13 <40654585+k0d13@users.noreply.github.com>
2026-04-18 01:24:52 +1200

    Refactor transformation logic in plugin-babel and plugin-unplugin to streamline code and improve readability

--- packages/plugin-babel/src/index.ts
+++ packages/plugin-babel/src/index.ts
@@ -3,26 +3,22 @@ import type { PluginObj } from '@babel/core';
 import { parse } from '@babel/parser';
 import { resolveConfig } from '@saykit/config/features/loader';
 
-// TODO: this does not need to be in this file, can move somewhere shared?
-function transformSource(id_: string, content: string) {
-  const config = resolveConfig();
-  const id = relative(process.cwd(), id_).replaceAll('\\', '/').split('?')[0]!;
-  const bucket = config.buckets.find((b) => b.match(id));
-  return bucket?.transformer.transform(id, content) ?? content;
-}
-
 export default function (): PluginObj {
+  const config = resolveConfig();
+
   return {
     name: 'saykit',
     visitor: {
       Program(path, state) {
-        const filename = state.file.opts.filename;
-        if (!filename || filename.includes('node_modules')) return;
+        const id_ = state.file.opts.filename;
+        if (!id_ || id_.includes('node_modules')) return;
+        const code = state.file.code ?? '';
 
-        const original = state.file.code ?? '';
-        const transformed = transformSource(filename, original);
+        const id = relative(process.cwd(), id_).replaceAll('\\', '/').split('?')[0]!;
+        const bucket = config.buckets.find((b) => b.match(id));
+        const transformed = bucket?.transformer.transform(id, code) ?? code;
 
-        if (transformed !== original) {
+        if (transformed !== code) {
           const ast = parse(transformed, state.file.opts.parserOpts as any);
           path.replaceWith(ast.program);
           path.skip();
